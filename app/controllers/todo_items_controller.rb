ac68659a5af1c67920b7028cf8cdf3424f1a4505
OTA Daichi <daichi.ota@access-company.com>
2013-07-26 10:41:43 +0900

    Remove update method

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -8,10 +8,6 @@ class TodoItemsController < ApplicationController
     redirect_to action: 'index'
   end
 
-  def update
-    redirect_to action: 'index'
-  end
-
   def destroy
     TodoItem.where(id: params[:id]).destroy_all
     redirect_to action: 'index'
